c0d0d2aefacb5709be2358622c922e631d93938f
Dilol87 <dilol87@vk.com>
2024-11-09 21:18:23 +0700

    тест вычислительного шейдера

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,44 @@
-#![allow(unused)]
-use vulkano::instance::{Instance, InstanceCreateInfo};
-use vulkano::VulkanLibrary;
-
-use vulkano::device::QueueFlags;
-use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo};
-
 use std::sync::Arc;
-
-use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage};
-use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter,StandardMemoryAllocator};
+use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
+use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage};
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+use vulkano::instance::{Instance, InstanceCreateInfo};
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
+use vulkano::pipeline::compute::ComputePipelineCreateInfo;
+use vulkano::pipeline::layout::PipelineDescriptorSetLayoutCreateInfo;
+use vulkano::pipeline::{ComputePipeline, PipelineLayout, PipelineShaderStageCreateInfo};
+use vulkano::sync::{self, GpuFuture};
+use vulkano::VulkanLibrary;
+use vulkano::pipeline::Pipeline;
+use vulkano::descriptor_set::{PersistentDescriptorSet, WriteDescriptorSet};
+use vulkano::descriptor_set::allocator::StandardDescriptorSetAllocator;
+use vulkano::command_buffer::allocator::{StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo};
+use vulkano::pipeline::PipelineBindPoint;
 
 fn main() {
-    //инициализация библиотеки
+    // Initialization
     let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
     let instance =
         Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
-    //выбор физического девайса
+
     let physical_device = instance
         .enumerate_physical_devices()
         .expect("could not enumerate devices")
         .next()
         .expect("no devices available");
-    //обработка и создание очередей на физическом устройстве
-    for family in physical_device.queue_family_properties() {
-        println!(
-            "Found a queue family with {:?} queue(s)",
-            family.queue_count
-        );
-    }
+
+    // Device creation
     let queue_family_index = physical_device
         .queue_family_properties()
         .iter()
         .enumerate()
-        .position(|(_queue_family_index, queue_family_properties)| {
-            queue_family_properties
-                .queue_flags
-                .contains(QueueFlags::GRAPHICS)
-        })
+        .position(|(_, q)| q.queue_flags.contains(QueueFlags::GRAPHICS))
         .expect("couldn't find a graphical queue family") as u32;
-    //создание логического девайса
+
     let (device, mut queues) = Device::new(
         physical_device,
         DeviceCreateInfo {
+            // here we pass the desired queue family to use by index
             queue_create_infos: vec![QueueCreateInfo {
                 queue_family_index,
                 ..Default::default()
@@ -50,15 +47,16 @@ fn main() {
         },
     )
     .expect("failed to create device");
-    //выбор конкретной очереди для использования в дальнейшем
+
     let queue = queues.next().unwrap();
-    //инициализация распределителя памяти
+
     let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
-    let iter = (0..128).map(|_| 5u8);
-    let buffer = Buffer::from_iter(
+
+    let data_iter = 0..65536u32;
+    let data_buffer = Buffer::from_iter(
         memory_allocator.clone(),
         BufferCreateInfo {
-            usage: BufferUsage::UNIFORM_BUFFER,
+            usage: BufferUsage::STORAGE_BUFFER,
             ..Default::default()
         },
         AllocationCreateInfo {
@@ -66,10 +64,96 @@ fn main() {
                 | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
             ..Default::default()
         },
-        iter,
+        data_iter,
+    )
+    .expect("failed to create buffer");
+
+    mod cs {
+        vulkano_shaders::shader! {
+            ty: "compute",
+            src: r"
+            #version 460
+
+            layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+
+            layout(set = 0, binding = 0) buffer Data {
+                uint data[];
+            } buf;
+
+            void main() {
+                uint idx = gl_GlobalInvocationID.x;
+                buf.data[idx] *= 12;
+            }
+        ",
+        }
+    }
+    let shader = cs::load(device.clone()).expect("failed to create shader module");
+    let cs = shader.entry_point("main").unwrap();
+    let stage = PipelineShaderStageCreateInfo::new(cs);
+    let layout = PipelineLayout::new(
+        device.clone(),
+        PipelineDescriptorSetLayoutCreateInfo::from_stages([&stage])
+            .into_pipeline_layout_create_info(device.clone())
+            .unwrap(),
+    )
+    .unwrap();
+    let compute_pipeline = ComputePipeline::new(
+        device.clone(),
+        None,
+        ComputePipelineCreateInfo::stage_layout(stage, layout),
+    )
+    .expect("failed to create compute pipeline");
+    let descriptor_set_allocator =
+        StandardDescriptorSetAllocator::new(device.clone(), Default::default());
+    let pipeline_layout = compute_pipeline.layout();
+    let descriptor_set_layouts = pipeline_layout.set_layouts();
+
+    let descriptor_set_layout_index = 0;
+    let descriptor_set_layout = descriptor_set_layouts
+        .get(descriptor_set_layout_index)
+        .unwrap();
+    let descriptor_set = PersistentDescriptorSet::new(
+        &descriptor_set_allocator,
+        descriptor_set_layout.clone(),
+        [WriteDescriptorSet::buffer(0, data_buffer.clone())], // 0 is the binding
+        [],
+    )
+    .unwrap();
+
+    let command_buffer_allocator = StandardCommandBufferAllocator::new(
+        device.clone(),
+        StandardCommandBufferAllocatorCreateInfo::default(),
+    );
+    let mut command_buffer_builder = AutoCommandBufferBuilder::primary(
+        &command_buffer_allocator,
+        queue.queue_family_index(),
+        CommandBufferUsage::OneTimeSubmit,
     )
         .unwrap();
-    let mut content = buffer.write().unwrap();
-    content[12] = 83;
-    content[7] = 3;
+    let work_group_counts = [1024, 1, 1];
+    command_buffer_builder
+        .bind_pipeline_compute(compute_pipeline.clone())
+        .unwrap()
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            compute_pipeline.layout().clone(),
+            descriptor_set_layout_index as u32,
+            descriptor_set,
+        )
+        .unwrap()
+        .dispatch(work_group_counts)
+        .unwrap();
+    let command_buffer = command_buffer_builder.build().unwrap();
+    let future = sync::now(device.clone())
+        .then_execute(queue.clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+    future.wait(None).unwrap();
+    let content = data_buffer.read().unwrap();
+    for (n, val) in content.iter().enumerate() {
+        assert_eq!(*val, n as u32 * 12);
+    }
+    println!("Everything succeeded!");
+    println!("data: {}", content[16]);
 }
